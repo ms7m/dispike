ad3c3aa007fbbb1acaeed3dbbaa4eeb35837521f
Mustafa Mohamed <ms7mohamed@gmail.com>
2020-12-23 13:54:12 -0500

    chore: update tests to account for embeds

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,14 +1,15 @@
 
-from dispike.response import DiscordStringResponse
+from dispike.response import DiscordResponse
+from dispike.helper.embed import Embed
 
 import pytest
 
 def test_string_response():
-    _created_content = DiscordStringResponse(content="test")
+    _created_content = DiscordResponse(content="test")
     assert _created_content.content == "test", _created_content.content
     assert _created_content.response['data']['content'] == "test", _created_content.content
 
-    _created_content = DiscordStringResponse(content="test", tts=True)
+    _created_content = DiscordResponse(content="test", tts=True)
     assert _created_content.content == "test"
     assert _created_content.tts == True
 
@@ -21,17 +22,29 @@ def test_string_response():
     assert _created_content.tts == False
     assert _created_content.response['data']['tts'] == False
 
+def test_response_with_embed():
+    _created_content = DiscordResponse(content="test")
+    _created_embed = Embed(title="Test")
+    _created_embed.add_field(name="test", value="test")
+    _created_content.add_new_embed(_created_embed)
+
+    assert isinstance(_created_content.embeds[0], dict) == True
+    assert _created_content.embeds[0] != {}
+
+def test_invalid_embed_object():
+    _created_content = DiscordResponse(content="test")
+    with pytest.raises(TypeError):
+        _created_content.add_new_embed(False)
+
+
 def test_invalid_content():
     with pytest.raises(TypeError):
-        _created_content = DiscordStringResponse(content=False)
+        _created_content = DiscordResponse(content=False)
+
+    _created_content = DiscordResponse(content='')
+    assert _created_content.content == None
 
     with pytest.raises(TypeError):
-        _created_content = DiscordStringResponse(content=None)
-
-    with pytest.raises(TypeError):
-        _created_content = DiscordStringResponse(content='')  
-
-    with pytest.raises(TypeError):
-        _created_content = DiscordStringResponse(content="Test", tts="invalid")
+        _created_content = DiscordResponse(content="Test", tts="invalid")
         
      
