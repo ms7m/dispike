e71096253ddc7722362b31c1363e3895f5dc7b51
Mustafa Mohamed <ms7mohamed@gmail.com>
2020-12-23 12:57:14 -0500

    new: add tests for dynamic arguments

--- tests/test_emitter.py
+++ tests/test_emitter.py
@@ -1,5 +1,6 @@
 
 from dispike.eventer import EventHandler
+from dispike.models.incoming import IncomingDiscordInteraction
 import pytest
 event_handler = EventHandler()
 
@@ -8,10 +9,60 @@ event_handler = EventHandler()
 async def dummy_function(*args, **kwargs):
     return kwargs.get('payload')
 
+@event_handler.on("sampleEventDynamicArgs")
+async def dummy_dynamic_function(dynamicargument, payload):
+    return dynamicargument
+
 @pytest.mark.asyncio
 async def test_event_handler():
     assert await event_handler.emit('sampleEvent', payload=True) == True
     assert "sampleEvent" in event_handler.callbacks
+
+@pytest.mark.asyncio
+async def test_dynamic_arguments_event_handler():
+    data = {	
+        "channel_id": "123123",	
+        "data": {	
+            "id": "12312312",	
+            "name": "sendmessage",	
+            "options": [{"name": "dynamicargument", "value": "dynamicargumentvalue"}],	
+        },	
+        "guild_id": "123123",	
+        "id": "123123123132",	
+        "member": {	
+            "deaf": False,	
+            "is_pending": False,	
+            "joined_at": "2019-05-12T18:36:16.878000+00:00",	
+            "mute": False,	
+            "nick": None,	
+            "pending": False,	
+            "permissions": "2147483647",	
+            "premium_since": None,	
+            "roles": [	
+                "123123",	
+                "123123",	
+                "1231233",	
+                "1231233133",	
+                "12412412414",	
+            ],	
+            "user": {	
+                "avatar": "b723979992a56",	
+                "discriminator": "3333",	
+                "id": "234234213122123",	
+                "public_flags": 768,	
+                "username": "exo",	
+            },	
+        },	
+        "token": "Null",	
+        "type": 2,	
+        "version": 1,	
+    }
+
+    discord_interaction = IncomingDiscordInteraction(**data)
+    dymanic_kwargs = {x.name: x.value for x in discord_interaction.data.options}
+    dymanic_kwargs['payload'] = discord_interaction
+    assert await event_handler.emit('sampleEventDynamicArgs', **dymanic_kwargs) == "dynamicargumentvalue"
+
 
 def test_non_async_event_handler():
     with pytest.raises(TypeError):
