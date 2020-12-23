98cd6311991ac50d127a4ecfca23879c7b42b918
Mustafa Mohamed <ms7mohamed@gmail.com>
2020-12-23 13:53:49 -0500

    feat: add support for embeds
    - also add support for determining whether command should "eat"
    users response.

--- dispike/response.py
+++ dispike/response.py
@@ -1,22 +1,47 @@
 
 from pydantic import BaseModel
+from .helper.embed import Embed
+import typing
 try:
     from typing import Literal
 except ImportError:
     # backport
     from typing_extensions import Literal
 
-class DiscordStringResponse(object):
-    def __init__(self, content: str = "Default String", tts: bool = False, **kwargs):
+class DiscordResponse(object):
+    def __init__(self, content: str = None, tts: bool = False, embeds: typing.List[Embed] = [], show_user_input: bool = False):
         
-        if isinstance(content, str) == False or content == "":
-            raise TypeError(f"content must be a string. recieved: {content}")
+        if content != None:
+            if isinstance(content, str) == False:
+                raise TypeError(f"Content must be a string")
+            elif content == "":
+                content = None
+                
+        #if isinstance(content, str) == False or content == "" or content != None:
+        #    raise TypeError(f"content must be a string. recieved: {content}")
 
         if isinstance(tts, bool) == False:
             raise TypeError("tts must be a bool")
 
         self._content = content
         self._tts = tts
+        self._embeds = [x.to_dict() for x in embeds]
+        if show_user_input == False:
+            self._type_response = 3
+        else:
+            self._type_response = 4
+
+
+    @property
+    def embeds(self):
+        return self._embeds
+
+    def add_new_embed(self, embed_to_add: Embed):
+        if isinstance(embed_to_add, Embed):
+            self._embeds.append(embed_to_add.to_dict())
+        else:
+            raise TypeError("embed must be a Embed object.")
+
 
     @property
     def content(self):
@@ -36,16 +61,19 @@ class DiscordStringResponse(object):
         
     @property
     def response(self):
-        if self.content == None:
-            raise TypeError("Content cannot be empty.")
+        if self.content == "":
+            self.content = None
 
         return {
-            "type": 4,
+            "type": self._type_response,
             "data": {
                 "tts": self.tts,
-                "content": self.content
+                "content": self.content,
+                "embeds": self.embeds
             }
         }
 
     def __call__(self):
         return self.response
+
+
