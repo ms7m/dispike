74bd65f5b798a7c6d928336ae6ec50fc422cc5a6
Mustafa Mohamed <git@ms7m.me>
2020-12-29 23:33:53 -0600

    feat(server): inspecting response func hints
    - will now look at event functions type hints to determine what
    to return. This allows for users to simply return DiscordResponse
    without adding .response

--- dispike/server.py
+++ dispike/server.py
@@ -6,9 +6,13 @@ from .eventer import EventHandler
 from .response import DiscordResponse, NotReadyResponse
 import json
 import typing
+from .helper.backport.cache import cache
 
 router = APIRouter()
 interaction = EventHandler()
+
+
+_RAISE_FOR_TESTING = False
 
 
 @router.post("/interactions")
@@ -34,8 +38,30 @@ async def handle_interactions(request: Request) -> Response:
 
     arguments["ctx"] = _parse_to_object
 
-    interaction_data = await interaction.emit(_parse_to_object.data.name, **arguments)
+    # Check the type hint for the return type, fallback for checking the type if no hints are provided
+    try:
+        _type_hinted_request = interaction.view_event_function_return_type(
+            _parse_to_object.data.name
+        )
+        _type_hinted_returned_value = _type_hinted_request["return"]
+        if _type_hinted_returned_value == DiscordResponse:
+            _get_res = await interaction.emit(_parse_to_object.data.name, **arguments)
+            return _get_res.response
+        elif _type_hinted_returned_value == NotReadyResponse:
+            return None
+        elif _type_hinted_returned_value == dict:
+            return await interaction.emit(_parse_to_object.data.name, **arguments)
+    except KeyError:
+        logger.error(
+            "unable to find return value for type hint.. resorting to guessing.."
+        )
+        if _RAISE_FOR_TESTING == True:
+            raise AssertionError("No hinting!")
+    except Exception:
+        logger.exception("unhandled exception for returning hinted value")
+        raise
 
+    interaction_data = await interaction.emit(_parse_to_object.data.name, **arguments)
     if isinstance(interaction_data, DiscordResponse):
         interaction_data: DiscordResponse
         return interaction_data.response
