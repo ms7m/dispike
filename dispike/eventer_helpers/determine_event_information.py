355b5325abdac17b6fd971229368e8525b9d25b2
Mustafa Mohamed <ms7mohamed@gmail.com>
2021-01-08 13:19:29 -0500

    refactor: determine_event_information for easier testing and higher cov.

--- dispike/eventer_helpers/determine_event_information.py
+++ dispike/eventer_helpers/determine_event_information.py
@@ -11,26 +11,26 @@ from loguru import logger
 def determine_event_information(
     interaction: IncomingDiscordInteraction,
 ) -> typing.Tuple[str, dict]:
-    if (
-        isinstance(interaction.data.options[0], SubcommandIncomingDiscordOptionList)
-        == True
-    ):
-        # subcommand event names will be must be xxx.xxx
-        _sub_command_arguments = {}
-        _event_name = f"{interaction.data.name}.{interaction.data.options[0].name}"
-        for argument in interaction.data.options[0].options:
-            _sub_command_arguments[argument.name] = argument.value
 
-        return _event_name, _sub_command_arguments
+    if isinstance(interaction, IncomingDiscordInteraction) == True:
+        if (
+            isinstance(interaction.data.options[0], SubcommandIncomingDiscordOptionList)
+            == True
+        ):
+            # subcommand event names will be must be xxx.xxx
+            _sub_command_arguments = {}
+            _event_name = f"{interaction.data.name}.{interaction.data.options[0].name}"
+            for argument in interaction.data.options[0].options:
+                _sub_command_arguments[argument.name] = argument.value
 
-    elif isinstance(interaction.data.options[0], IncomingDiscordOption) == True:
-        _command_arguments = {}
+            return _event_name, _sub_command_arguments
 
-        for argument in interaction.data.options:
-            argument: IncomingDiscordOption
-            _command_arguments[argument.name] = argument.value
+        elif isinstance(interaction.data.options[0], IncomingDiscordOption) == True:
+            _command_arguments = {}
 
-        return interaction.data.name, _command_arguments
+            for argument in interaction.data.options:
+                argument: IncomingDiscordOption
+                _command_arguments[argument.name] = argument.value
 
-    else:
-        raise TypeError("unable to determine eventname.")
+            return interaction.data.name, _command_arguments
+    raise TypeError("unable to determine eventname.")
