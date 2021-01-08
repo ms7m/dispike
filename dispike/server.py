696e83f35ebbe21d15a07407ec57d9d219eb7e5b
Mustafa Mohamed <ms7mohamed@gmail.com>
2021-01-07 20:10:43 -0500

    refactor: use external function for event name/args, remove notready

--- dispike/server.py
+++ dispike/server.py
@@ -1,12 +1,19 @@
-from fastapi import APIRouter, Request, Response
+from fastapi import APIRouter, Request, Response, BackgroundTasks
 from fastapi.responses import PlainTextResponse
 from loguru import logger
 from .middlewares.verification import DiscordVerificationMiddleware
-from .models.incoming import IncomingDiscordInteraction
+from .models.incoming import (
+    IncomingDiscordInteraction,
+    IncomingDiscordOptionList,
+    SubcommandIncomingDiscordOptionList,
+    IncomingDiscordOption,
+)
 from .eventer import EventHandler
-from .response import DiscordResponse, NotReadyResponse
+from .eventer_helpers.determine_event_information import determine_event_information
+from .response import DiscordResponse
 import json
 import typing
+
 
 router = APIRouter()
 interaction = EventHandler()
@@ -22,6 +29,10 @@ async def ping():
     )
 
 
+async def proxy_to_event_func(interaction: EventHandler, event_name: str, arguments):
+    await interaction.emit(event_name, **arguments)
+
+
 @router.post("/interactions")
 async def handle_interactions(request: Request) -> Response:
     logger.info("interaction recieved.")
@@ -33,35 +44,27 @@ async def handle_interactions(request: Request) -> Response:
         return {"type": 1}
 
     _parse_to_object = IncomingDiscordInteraction(**_get_request_body)
-
-    if interaction.check_event_exists(_parse_to_object.data.name) == False:
+    _event_name, arguments = determine_event_information(_parse_to_object)
+    logger.info(f"event name: {_event_name}")
+    if interaction.check_event_exists(_event_name) == False:
         logger.debug("discarding event not existing.")
-        return {}
+        return {"type": 5}
 
-    if len(_parse_to_object.data.options) > 0:
-        arguments = {x.name: x.value for x in _parse_to_object.data.options}
-    else:
-        arguments = {}
+    _event_settings = interaction.return_event_settings(_event_name)
 
     arguments["ctx"] = _parse_to_object
 
     # Check the type hint for the return type, fallback for checking the type if no hints are provided
     try:
-        _type_hinted_request = interaction.view_event_function_return_type(
-            _parse_to_object.data.name
-        )
+        _type_hinted_request = interaction.view_event_function_return_type(_event_name)
         _type_hinted_returned_value = _type_hinted_request["return"]
         if _type_hinted_returned_value == DiscordResponse:
-            _get_res = await interaction.emit(_parse_to_object.data.name, **arguments)
+            _get_res = await interaction.emit(_event_name, **arguments)
+
+            logger.debug(_get_res.response)
             return _get_res.response
-        elif _type_hinted_returned_value == NotReadyResponse:
-            # TODO: Offer a setting to change the type for simply acknowledging a commmand
-            # per https://discord.com/developers/docs/interactions/slash-commands#interaction-response-interactionresponsetype
-            return {
-                "type": 2
-            }
         elif _type_hinted_returned_value == dict:
-            return await interaction.emit(_parse_to_object.data.name, **arguments)
+            return await interaction.emit(_event_name, **arguments)
     except KeyError:
         logger.error(
             "unable to find return value for type hint.. resorting to guessing.."
@@ -72,7 +75,7 @@ async def handle_interactions(request: Request) -> Response:
         logger.exception("unhandled exception for returning hinted value")
         raise
 
-    interaction_data = await interaction.emit(_parse_to_object.data.name, **arguments)
+    interaction_data = await interaction.emit(_event_name, **arguments)
     if isinstance(interaction_data, DiscordResponse):
         interaction_data: DiscordResponse
         return interaction_data.response
@@ -80,13 +83,5 @@ async def handle_interactions(request: Request) -> Response:
     if isinstance(interaction_data, dict):
         return interaction_data
 
-    if isinstance(interaction_data, NotReadyResponse):
-        return {
-            "type": 2
-        }
-
-
     # Backup response, simply acknowledge. (Type 5)
-    return {
-        "type": 5
-    }
+    return {"type": 5}
