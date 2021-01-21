6c76a735d1f582658a367c69c86fce889bf08b92
Mustafa Mohamed <ms7mohamed@gmail.com>
2021-01-21 18:13:14 -0500

    tests: add more tests for delete_command, edit_command
    - add more tests for bulk edit commands
    - add tests for deleting commands.

--- tests/test_network_calls.py
+++ tests/test_network_calls.py
@@ -252,6 +252,69 @@ def example_edit_command():
 
 
 @respx.mock
+def test_bulk_edit_command_guild_only(
+    dispike_object: Dispike, example_edit_command: DiscordCommand
+):
+    respx.put(
+        "https://discord.com/api/v8/applications/APPID/guilds/EXAMPLE_GUILD/commands"
+    ).mock(
+        return_value=Response(
+            200,
+            json=[example_edit_command.dict(), example_edit_command.dict()],
+        )
+    )
+    _edit_command = dispike_object.edit_command(
+        new_command=[example_edit_command, example_edit_command],
+        guild_only=True,
+        bulk=True,
+        guild_id_passed="EXAMPLE_GUILD",
+    )
+    assert isinstance(_edit_command, list) == True
+    assert len(_edit_command) == 2
+    for command in _edit_command:
+        assert isinstance(command, DiscordCommand)
+        assert command.id == example_edit_command.id
+        assert command.name == example_edit_command.name
+
+
+@respx.mock
+def test_bulk_edit_command_globally(
+    dispike_object: Dispike, example_edit_command: DiscordCommand
+):
+    respx.patch("https://discord.com/api/v8/applications/APPID/commands").mock(
+        return_value=Response(
+            200,
+            json=[example_edit_command.dict(), example_edit_command.dict()],
+        )
+    )
+    _edit_command = dispike_object.edit_command(
+        new_command=example_edit_command, bulk=True
+    )
+    assert isinstance(_edit_command, list) == True
+    assert len(_edit_command) == 2
+    for command in _edit_command:
+        assert isinstance(command, DiscordCommand)
+        assert command.id == example_edit_command.id
+        assert command.name == example_edit_command.name
+
+
+@respx.mock
+def test_single_edit_command_globally(
+    dispike_object: Dispike, example_edit_command: DiscordCommand
+):
+    respx.patch("https://discord.com/api/v8/applications/APPID/commands").mock(
+        return_value=Response(
+            200,
+            json=example_edit_command.dict(),
+        )
+    )
+    _edit_command = dispike_object.edit_command(new_command=example_edit_command)
+    assert isinstance(_edit_command, DiscordCommand) == True, type(_edit_command)
+    assert _edit_command.id == example_edit_command.id
+    assert _edit_command.name == example_edit_command.name
+
+
+@respx.mock
 def test_single_edit_command_guild_only(
     dispike_object: Dispike, example_edit_command: DiscordCommand
 ):
@@ -273,6 +336,74 @@ def test_single_edit_command_guild_only(
     assert isinstance(_edit_command, DiscordCommand) == True
     assert _edit_command.id == example_edit_command.id
     assert _edit_command.name == example_edit_command.name
+
+
+@respx.mock
+def test_delete_command_guild_only(
+    dispike_object: Dispike, example_edit_command: DiscordCommand
+):
+    respx.delete(
+        "https://discord.com/api/v8/applications/APPID/guilds/EXAMPLE_GUILD/commands/1234"
+    ).mock(
+        return_value=Response(
+            204,
+            json=example_edit_command.dict(),
+        )
+    )
+    _delete_command = dispike_object.delete_command(
+        command_id=1234,
+        guild_only=True,
+        guild_id_passed="EXAMPLE_GUILD",
+    )
+    assert _delete_command == True
+
+
+@respx.mock
+def test_failed_delete_command_guild_only(
+    dispike_object: Dispike, example_edit_command: DiscordCommand
+):
+    respx.delete(
+        "https://discord.com/api/v8/applications/APPID/guilds/EXAMPLE_GUILD/commands/1234"
+    ).mock(
+        return_value=Response(
+            500,
+            json=example_edit_command.dict(),
+        )
+    )
+
+    with pytest.raises(DiscordAPIError):
+        _delete_command = dispike_object.delete_command(
+            command_id=1234,
+            guild_only=True,
+            guild_id_passed="EXAMPLE_GUILD",
+        )
+
+
+@respx.mock
+def test_delete_command_globally(
+    dispike_object: Dispike,
+):
+    respx.delete("https://discord.com/api/v8/applications/APPID/commands/1234").mock(
+        return_value=Response(
+            204,
+        )
+    )
+
+    _delete_command = dispike_object.delete_command(command_id=1234)
+    assert _delete_command == True
+
+
+@respx.mock
+def test_failed_delete_command_globally(
+    dispike_object: Dispike,
+):
+    respx.delete("https://discord.com/api/v8/applications/APPID/commands/1234").mock(
+        return_value=Response(
+            500,
+        )
+    )
+    with pytest.raises(DiscordAPIError):
+        _delete_command = dispike_object.delete_command(command_id=1234)
 
 
 def test_get_commands_invalid_guild_id_passed(dispike_object: Dispike):
