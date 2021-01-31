a903a27cb07b5062a51232261cd4dbaa43066d53
Mustafa Mohamed <ms7mohamed@gmail.com>
2021-01-31 02:52:55 -0500

    fix: fix issue with subcommand interaction raising ValidationError.
    - adds new model called SubcommandIncomingDiscordOptionListChild

--- dispike/eventer_helpers/determine_event_information.py
+++ dispike/eventer_helpers/determine_event_information.py
@@ -3,6 +3,7 @@ from ..models.incoming import (
     IncomingDiscordOptionList,
     SubcommandIncomingDiscordOptionList,
     IncomingDiscordOption,
+    SubcommandIncomingDiscordOptionListChild,
 )
 import typing
 from loguru import logger
@@ -13,15 +14,34 @@ def determine_event_information(
 ) -> typing.Tuple[str, dict]:
 
     if isinstance(interaction, IncomingDiscordInteraction) == True:
+
+        if interaction.data.options == None:
+            return interaction.data.name, {}
+
         if (
             isinstance(interaction.data.options[0], SubcommandIncomingDiscordOptionList)
             == True
         ):
             # subcommand event names will be must be xxx.xxx
             _sub_command_arguments = {}
-            _event_name = f"{interaction.data.name}.{interaction.data.options[0].name}"
+
+            if hasattr(interaction.data.options[0], "options"):
+                # gosh this is hella ugly, i need to rewrite this.
+                _event_name = f"{interaction.data.name}.{interaction.data.options[0].name}.{interaction.data.options[0].options[0].name}"
+            else:
+                _event_name = (
+                    f"{interaction.data.name}.{interaction.data.options[0].name}"
+                )
             for argument in interaction.data.options[0].options:
-                _sub_command_arguments[argument.name] = argument.value
+
+                if isinstance(argument, SubcommandIncomingDiscordOptionListChild):
+                    for sub_child_argument in argument.options:
+                        _sub_command_arguments[
+                            sub_child_argument.name
+                        ] = sub_child_argument.value
+                    break
+                else:
+                    _sub_command_arguments[argument.name] = argument.value
 
             return _event_name, _sub_command_arguments
 
@@ -33,4 +53,5 @@ def determine_event_information(
                 _command_arguments[argument.name] = argument.value
 
             return interaction.data.name, _command_arguments
+
     raise TypeError("unable to determine eventname.")
