5b766b9c237065e1f61144e1c18f1bd0aa51542f
Mustafa <mustafa@ms7m.me>
2021-05-30 00:28:58 -0500

    feat: add models for permissions #36

---
+++ dispike/register/models/permissions.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from enum import Enum
+import typing
+
+
+class ApplicationCommandPermissionType(int, Enum):
+    ROLE = 1
+    USER = 2
+
+
+class ApplicationCommandPermissions(BaseModel):
+    id: int
+    type: ApplicationCommandPermissionType
+    permission: bool
+
+
+class GuildApplicationCommandPermissions(BaseModel):
+    id: int
+    application_id: int
+    permissions: typing.List[ApplicationCommandPermissionType]
