4f3b16f55da2d8a81e43564497b7bbb094b7f02a
Mustafa <mustafa@ms7m.me>
2021-05-30 13:35:28 -0500

    fix: add type hinting for VSCode for permissions fix: fixpermissions having the wrong hint

--- dispike/register/models/permissions.py
+++ dispike/register/models/permissions.py
@@ -1,24 +1,36 @@
 from pydantic import BaseModel
 from enum import Enum
 import typing
+import dataclasses
+
+if typing.TYPE_CHECKING:  # pragma: no cover
+    static_check_init_args = dataclasses.dataclass
+else:
+
+    def static_check_init_args(cls):
+        return cls
 
 
+@static_check_init_args
 class ApplicationCommandPermissionType(int, Enum):
     ROLE = 1
     USER = 2
 
 
+@static_check_init_args
 class ApplicationCommandPermissions(BaseModel):
     id: int
     type: ApplicationCommandPermissionType
     permission: bool
 
 
+@static_check_init_args
 class GuildApplicationCommandPermissions(BaseModel):
     id: int
     application_id: int
-    permissions: typing.List[ApplicationCommandPermissionType]
+    permissions: typing.List[ApplicationCommandPermissions]
 
 
+@static_check_init_args
 class NewApplicationPermission(BaseModel):
     permissions: typing.List[ApplicationCommandPermissions]
