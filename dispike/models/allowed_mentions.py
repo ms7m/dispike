86ad4864f8aacd717cd095ef23c1796ea876a124
Mustafa <mustafa@ms7m.me>
2021-05-30 21:46:08 -0500

    feat: add models for allowedmentions #38

---
+++ dispike/models/allowed_mentions.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from pydantic import BaseModel, conlist
+import typing
+import dataclasses
+
+if typing.TYPE_CHECKING:  # pragma: no cover
+    static_check_init_args = dataclasses.dataclass
+else:
+
+    def static_check_init_args(cls):
+        return cls
+
+
+class AllowedMentionTypes(str, Enum):
+    ROLE_MENTIONS = "roles"
+    USER_MENTIONS = "users"
+    EVERYONE_MENTIONS = "everyone"
+
+
+@static_check_init_args
+class AllowedMentions(BaseModel):
+    parse: typing.List[AllowedMentionTypes]
+    roles: conlist(AllowedMentionTypes, max_items=100)
+    users: conlist(AllowedMentionTypes, max_items=100)
+    replied_user: bool = False
